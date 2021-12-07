76a1c692846b28c8fc931c3ceb9978c12a4004bd
PatrickSchifter <95550051+PatrickSchifter@users.noreply.github.com>
2021-12-07 11:29:45 -0300

    Hoje

--- Automação de rotina diária/SSW.py
+++ Automação de rotina diária/SSW.py
@@ -1,78 +1,81 @@
 import requests
 import pandas as pd
+from Variaveis import itens_ign, all_index, indexes, meses, mes_atual, ano_atual
 
 url = 'https://ssw.inf.br/api/tracking'
 cnpj = '00069957000194'
 path = r'K:/CWB/Logistica/Rastreamento/Controle_Monitoramento/MONITORAMENTO 2021 v.1.2.xlsx'
-itens_ign = ['tracking; ', 'success; ', 'message; ', 'header; ', 'remetente; ', 'destinatario; ', 'items; ',
-             'item; ',
-             'data_hora; ', 'dominio; ', 'filial; ', 'cidade; ', 'ocorrencia; ', 'descricao; ', 'tipo; ',
-             'data_hora_efetiva; ', 'nome_recebedor; ', 'nro_doc_recebedor; ',
-             '?xml version="1.0" encoding="UTF-8" ?; ', '']
-all_index = ['Número', 'N.Pré-Nota', 'Emissão', 'Fantasia-Destinatário', 'Cidade-Destinatário', 'Uf',
-             'Natureza-Fiscal', 'Situação-Fiscal', 'Descrição-Do-Depósito', 'Fantasia_Do_Transportador',
-             'Fantasia-Comissionado', 'Data-De-Coleta', 'Previsão-Entrega', 'D_Entrega', 'Agendamento',
-             'Lead-Time',
-             'Dias-Para-Entrega', 'Resumo', 'Observações']
-indexes = ['Data', 'Transportadora', 'Filial', 'Cidade/Estado', 'Ult_Status']
+name_m_atual = meses[str(int(mes_atual))].upper() + '-' + str(ano_atual)
+name_m_ant = meses[str(int(mes_atual) - 1)].upper() + '-' + str(ano_atual)
 
-df = pd.read_excel(path, 'NOVEMBRO-2021')
 
-df.columns = all_index
-df.fillna(0)
-df = df.query(
-    "Fantasia_Do_Transportador != 'MALBEC' & Fantasia_Do_Transportador != 'URANOLOG' &\
-    Fantasia_Do_Transportador != 'JOHNKE TRANSPORTES' & Fantasia_Do_Transportador > '0' &\
-     Fantasia_Do_Transportador != 'ALIANCA' & Fantasia_Do_Transportador != 'GRANDE ADEGA - MATRIZ' &\
-      Fantasia_Do_Transportador != 'TRANSFRIOS TRANSP' & Fantasia_Do_Transportador != 'BRINGER DO BRASIL' &\
-       Fantasia_Do_Transportador != 'GRANDE ADEGA' & Fantasia_Do_Transportador != 'TRANSFRIOS SP' & D_Entrega == '-'")
-nf = []
+def search_data(mes):
+    df = pd.read_excel(path, mes)
+    df.columns = all_index
+    df.fillna(0)
+    df = df.query(
+        "Fantasia_Do_Transportador != 'MALBEC' & Fantasia_Do_Transportador != 'URANOLOG' &\
+        Fantasia_Do_Transportador != 'JOHNKE TRANSPORTES' & Fantasia_Do_Transportador > '0' &\
+         Fantasia_Do_Transportador != 'ALIANCA' & Fantasia_Do_Transportador != 'GRANDE ADEGA - MATRIZ' &\
+          Fantasia_Do_Transportador != 'TRANSFRIOS TRANSP' & Fantasia_Do_Transportador != 'BRINGER DO BRASIL' &\
+           Fantasia_Do_Transportador != 'GRANDE ADEGA' & Fantasia_Do_Transportador != 'TRANSFRIOS SP' & D_Entrega == '-'")
+    nf = []
 
-for x in df['Número']:
+    for x in df['Número']:
 
-    nro_nf = x
+        nro_nf = x
 
-    request = requests.post(url=url, data={'cnpj': cnpj, 'nro_nf': nro_nf})
+        request = requests.post(url=url, data={'cnpj': cnpj, 'nro_nf': nro_nf})
 
-    info = []
-    inf = []
-    cont = 0
-    for stat in request.text.split(sep='<'):
-
-        stat = stat.replace('/', '')
-        stat = stat.replace('>', '; ')
-        if 'success; false' in stat:
-            continue
-        if stat in itens_ign:
-            continue
-        elif 'tipo' in stat:
-            continue
-        elif 'tracking' in stat:
-            continue
-        elif 'message' in stat:
-            continue
-        elif 'remetente' in stat:
-            continue
-        elif 'destinatario' in stat:
-            continue
-        elif 'success' in stat:
-            continue
-        elif 'efetiva' in stat:
-            continue
-        stat = stat.split(';')
-        if cont == 0:
-            inf.append(nro_nf)
-        if cont == 5:
-            cont = 0
-            info.append(inf)
-            inf = []
-        else:
-            inf.append(stat[1])
-            cont += 1
-    try:
-        nf.append(info[-1])
-    except IndexError:
-        pass
         info = []
+        inf = []
+        cont = 0
+        for stat in request.text.split(sep='<'):
 
-df_ssw = pd.DataFrame(data=df, index=indexes)
+            stat = stat.replace('/', '')
+            stat = stat.replace('>', '; ')
+            if 'success; false' in stat:
+                continue
+            if stat in itens_ign:
+                continue
+            elif 'tipo' in stat:
+                continue
+            elif 'tracking' in stat:
+                continue
+            elif 'message' in stat:
+                continue
+            elif 'remetente' in stat:
+                continue
+            elif 'destinatario' in stat:
+                continue
+            elif 'success' in stat:
+                continue
+            elif 'efetiva' in stat:
+                continue
+            stat = stat.split(';')
+            if cont == 0:
+                inf.append(nro_nf)
+            if cont == 5:
+                cont = 0
+                info.append(inf)
+                inf = []
+            else:
+                inf.append(stat[1])
+                cont += 1
+        try:
+            nf.append(info[-1])
+        except IndexError:
+            pass
+            info = []
+
+    df_ssw = pd.DataFrame(data=nf, columns=indexes)
+    print(df_ssw)
+    writer = pd.ExcelWriter(path=r'K:/CWB/Logistica/Rastreamento/Patrick/Teste/teste' + mes + '.xlsx', engine='openpyxl')
+    df_ssw.to_excel(excel_writer=writer, index=False)
+    writer.save()
+
+    return df_ssw
+
+
+search_data(name_m_atual)
+search_data(name_m_ant)
--- Automação de rotina diária/Variaveis.py
+++ Automação de rotina diária/Variaveis.py
@@ -2,11 +2,11 @@ from datetime import datetime
 import datetime
 from datetime import date
 
-dia_atual = datetime.today().strftime('%d')
-ano_atual = datetime.today().strftime('%Y')
-mes_atual = datetime.today().strftime('%m')
+dia_atual = date.today().strftime('%d')
+ano_atual = date.today().strftime('%Y')
+mes_atual = date.today().strftime('%m')
 
-today = datetime.today().strftime("%d-%m-%Y")
+today = date.today().strftime("%d-%m-%Y")
 partial_path = "K:/CWB/Logistica/Rastreamento/Patrick/Storage/"
 path_dir_tod = partial_path + today
 
@@ -15,6 +15,8 @@ all_index = ['Número', 'N.Pré-Nota', 'Emissão', 'Fantasia-Destinatário', 'Ci
              'Fantasia-Comissionado', 'Data-De-Coleta', 'Previsão-Entrega', 'D_Entrega', 'Agendamento',
              'Lead-Time',
              'Dias-Para-Entrega', 'Resumo', 'Observações']
+
+indexes = ['Número', 'Data', 'Transportadora', 'Filial', 'Cidade/Estado', 'Ult_Status']
 
 dest_file = r'K:/CWB/Logistica/Rastreamento/Controle de Monitoramento/Automação de Monitoramento/MONITORAMENTO ' + ano_atual + '.xlsx'
 
@@ -34,7 +36,6 @@ dias_meses = {
 }
 
 dia_hoje = int(datetime.date.today().strftime("%d"))
-mes_atual = int(datetime.date.today().strftime("%m"))
 dia_semana = datetime.date.weekday(datetime.date.today())
 ano_tual = int(datetime.date.today().strftime("%Y"))
 
@@ -42,54 +43,55 @@ ano_tual = int(datetime.date.today().strftime("%Y"))
 def data():
     if dia_semana == 0:
         if dia_hoje == 1:
-            dia = dias_meses[mes_atual - 1] - 2
-            return datetime.date(ano_tual, mes_atual - 1, dia).strftime("%d/%m/%Y")
+            dia = dias_meses[int(mes_atual) - 1] - 2
+            return datetime.date(ano_tual, int(mes_atual) - 1, dia).strftime("%d/%m/%Y")
 
         elif dia_hoje == 2:
-            dia = dias_meses[mes_atual - 1] - 1
-            return datetime.date(ano_tual, mes_atual - 1, dia).strftime("%d/%m/%Y")
+            dia = dias_meses[int(mes_atual) - 1] - 1
+            return datetime.date(ano_tual, int(mes_atual) - 1, dia).strftime("%d/%m/%Y")
 
         elif dia_hoje == 3:
             dia = dias_meses[mes_atual]
-            return datetime.date(ano_tual, mes_atual - 1, dia).strftime("%d/%m/%Y")
+            return datetime.date(ano_tual, int(mes_atual) - 1, dia).strftime("%d/%m/%Y")
 
         else:
             dia = dia_hoje - 3
-            return datetime.date(ano_tual, mes_atual, dia).strftime("%d/%m/%Y")
+            return datetime.date(ano_tual, int(mes_atual), dia).strftime("%d/%m/%Y")
 
     else:
         if dia_hoje == 1:
-            dia = dias_meses[mes_atual - 1]
-            return datetime.date(ano_tual, mes_atual - 1, dia).strftime("%d/%m/%Y")
+            dia = dias_meses[int(mes_atual) - 1]
+            return datetime.date(ano_tual, int(mes_atual) - 1, dia).strftime("%d/%m/%Y")
         else:
             dia = dia_hoje - 1
-            return datetime.date(ano_tual, mes_atual, dia).strftime("%d/%m/%Y")
+            return datetime.date(ano_tual, int(mes_atual), dia).strftime("%d/%m/%Y")
 
 
 def dataf():
     if dia_semana == 0:
         if dia_hoje == 1:
-            dia = dias_meses[mes_atual - 1] - 2
-            return datetime.date(ano_tual, mes_atual - 1, dia).strftime("%d-%m-%Y")
+            dia = dias_meses[int(mes_atual) - 1] - 2
+            return datetime.date(ano_tual, int(mes_atual) - 1, dia).strftime("%d-%m-%Y")
 
         elif dia_hoje == 2:
-            dia = dias_meses[mes_atual - 1] - 1
-            return datetime.date(ano_tual, mes_atual - 1, dia).strftime("%d-%m-%Y")
+            dia = dias_meses[int(mes_atual) - 1] - 1
+            return datetime.date(ano_tual, int(mes_atual) - 1, dia).strftime("%d-%m-%Y")
 
         elif dia_hoje == 3:
-            dia = dias_meses[mes_atual]
-            return datetime.date(ano_tual, mes_atual - 1, dia).strftime("%d-%m-%Y")
+            dia = dias_meses[int(mes_atual)]
+            return datetime.date(ano_tual, int(mes_atual) - 1, dia).strftime("%d-%m-%Y")
 
         else:
             dia = dia_hoje - 3
-            return datetime.date(ano_tual, mes_atual, dia).strftime("%d-%m-%Y")
+            return datetime.date(ano_tual, int(mes_atual), dia).strftime("%d-%m-%Y")
     else:
         if dia_hoje == 1:
-            dia = dias_meses[mes_atual - 1]
-            return datetime.date(ano_tual, mes_atual - 1, dia).strftime("%d-%m-%Y")
+            dia = dias_meses[int(mes_atual) - 1]
+            return datetime.date(ano_tual, int(mes_atual) - 1, dia).strftime("%d-%m-%Y")
         else:
             dia = dia_hoje - 1
-            return datetime.date(ano_tual, mes_atual, dia).strftime("%d-%m-%Y")
+            return datetime.date(ano_tual, int(mes_atual), dia).strftime("%d-%m-%Y")
+
 
 meses = {"01": "janeiro",
          "02": "fevereiro",
@@ -110,4 +112,10 @@ today = date.today().strftime("%d-%m-%Y")
 dest_path = "K:/CWB/Logistica/Rastreamento/Patrick/Storage/" + today
 dia_ontem = int(date.today().strftime('%d')) - 1
 ano_atual = int(date.today().strftime('%Y'))
-yest = date(ano_atual, mes_atual, dia_ontem).strftime("%d-%m-%Y")
+yest = date(ano_atual, int(mes_atual), dia_ontem).strftime("%d-%m-%Y")
+
+itens_ign = ['tracking; ', 'success; ', 'message; ', 'header; ', 'remetente; ', 'destinatario; ', 'items; ',
+             'item; ',
+             'data_hora; ', 'dominio; ', 'filial; ', 'cidade; ', 'ocorrencia; ', 'descricao; ', 'tipo; ',
+             'data_hora_efetiva; ', 'nome_recebedor; ', 'nro_doc_recebedor; ',
+             '?xml version="1.0" encoding="UTF-8" ?; ', '']
